310e05d97aa174260a9d8d68de4de59b7e81ea5d
zendl0r <d.zendler@gmx.de>
2019-04-11 22:35:58 +0200

    Update extract_fasta.py

--- extract_fasta.py
+++ extract_fasta.py
@@ -6,16 +6,15 @@
 #Tested on python version 2.7.15_1, MacOS High Sierra 10.13.6
 
 from __future__ import print_function
-import sys, os, datetime
+import sys, os, datetime, time
 #------------------------------------------------------------------------------
 #Taken from: https://www.agnosticdev.com/content/how-open-file-python
 #Matt Eaton on Sun, 12/17/2017 - 10:35 PM
 class File():
-
     def __init__(self):
         #Initializing the variables for the file
-        self.file_lines1 = []
-        self.file_lines2 = []
+        self.file_lines_seqIDs = []
+        self.file_lines_multiFasta = []
         self.file_input_1 = ""
         self.file_input_2 = ""
         self.complete_content1 = ""
@@ -27,97 +26,89 @@ class File():
 
         #Check if there is a filename indicated
         self.parse_stdin()
-        if self.file_input_name1 is not "" or self.file_input_stdin1 is not None:
+        if self.file_input_seqID is not "" or self.file_input_stdin1 is not None:
             self.read_content_of_file()
         else:
-            exit("Somthing went wrong with parsing your input file. Exiting. 1")
-        if len(self.file_lines1) == 0:
+            exit("Somthing went wrong with parsing your input file. Exiting. SeqIDs")
+        if len(self.file_lines_seqIDs) == 0:
             exit("Nothing to read here. Exiting")
 
-        if self.file_input_name2 is not "" or self.file_input_stdin2 is not None:
+        if self.file_input_multiFasta is not "" or self.file_input_stdin2 is not None:
             self.read_content_of_file()
         else:
-            exit("Somthing went wrong with parsing your input file. Exiting. 2")
-        if len(self.file_lines2) == 0:
+            exit("Somthing went wrong with parsing your input file. Exiting. Fasta")
+        if len(self.file_lines_multiFasta) == 0:
             exit("Nothing to read here. Exiting")
-
 
     def parse_stdin(self):
         if len(sys.argv) == 3 and sys.argv[1] is not None:
-            self.file_input_name1 = sys.argv[1]
-            self.file_input_name2 = sys.argv[2]
+            self.file_input_seqID = sys.argv[1]
+            self.file_input_multiFasta = sys.argv[2]
             self.working_path = os.getcwd()
         elif not sys.stdin.isatty():
             self.file_input_stdin1 = sys.stdin
         else:
-            print("There was an issue locating your input.")
-            exit("Please provide a valid filename/file as input")
+            print("A problem occured.\n")
+            print("Calling the script: python seqIDs_file multiFasta.fa\n")
+            print("Contact: d.zendler@gmx.de")
+            exit("Please provide a valid list of seqIDS and a valid Fasta file")
 
     def read_content_of_file(self):
         try:
             if self.file_input_stdin1 is None:
-                    with open(self.file_input_name1, 'r') as file_obj:
+                    with open(self.file_input_seqID, 'r') as file_obj:
                         self.complete_content1 = file_obj.read()
             else:
                 self.complete_content1 = self.file_input_stdin1.read()
-            self.file_lines1 = self.complete_content1.split("\n")
+            self.file_lines_seqIDs = self.complete_content1.split("\n")
 
             if self.file_input_stdin2 is None:
-                    with open(self.file_input_name2, 'r') as file_obj:
+                    with open(self.file_input_multiFasta, 'r') as file_obj:
                         self.complete_content2 = file_obj.read()
             else:
                 self.complete_content2 = self.file_input_stdin2.read()
-            self.file_lines2 = self.complete_content2.split("\n")
-
+            self.file_lines_multiFasta = self.complete_content2.split()
         except ValueError:
             error_raised = "Error loading file: " + str(sys.exc_info()[0])
             print("This program needs an input file to continue. Exiting")
-
-#Taken from: https://www.agnosticdev.com/content/how-open-file-python
-#Matt Eaton on Sun, 12/17/2017 - 10:35 PM
-#------------------------------------------------------------------------------
-def main():
-    #Call the current date and time
-    now = datetime.datetime.now()
-    #------------------------------------------------------------
-    #Call the input files
+#function to find the index of the sequences to be extracted--------------------
+def findIndexOfSeq(seqID):
     file = File()
-    #------------------------------------------------------------
-    seq = ""
-    a = 0
-    #Generating the outputfile
-    out = open(file.working_path + '/out.fa', 'w')
-    out.write(";" + str(now) + "\n")
-    #------------------------------------------------------------
-    #go through the sequencenames to be extracted
-    for lines in file.file_lines1:
-        #if the line contains something write '>' in front of it
-        if lines != "":
-            lines = ">" + lines
-            #search the index of the matching sequence name in the fasta file
-            a = file.file_lines2.index(lines)
-            #write the sequence name in seq
-            seq = seq + file.file_lines2[a] + "\n"
-            #count the index one further to write the Nt or AA sequence in seq
-            a = a + 1
-            while a < len(file.file_lines2):
-                #as long as there is no new '>' the sequence belongs to the
-                #sequence name match
-                if '>' not in file.file_lines2[a]:
-                    seq = seq + file.file_lines2[a] + "\n"
-                    a = a + 1
-                else:
-                    #as soon as a new '>' comes a new sequence starts and the
-                    #for loop needs to continue with the next sequence name
-                    break
-        else:
-            break
-    #------------------------------------------------------------
-    #write the whole sequences in terminal and in file
-    print(seq)
+    indexList = []
+    for line in seqID:
+        if line != "":
+            line = ">" + line
+            indexList.append(file.file_lines_multiFasta.index(line))
+    return indexList
+#function to extract the sequences----------------------------------------------
+def extractSeqs(indexList):
+    file = File()
+    seq = ''
+    for ind in indexList:
+        seq = seq + file.file_lines_multiFasta[ind] + "\n"
+        ind = ind + 1
+        while ind < len(file.file_lines_multiFasta):
+            if '>' not in file.file_lines_multiFasta[ind]:
+                seq = seq + file.file_lines_multiFasta[ind] + "\n"
+                ind = ind + 1
+            else:
+                break
+    return seq
+#get the job done---------------------------------------------------------------
+def main():
+#Call the current date and time-------------------------------------------------
+    now = datetime.datetime.now()
+#Call the input files-----------------------------------------------------------
+    file = File()
+#Generating the outputfile------------------------------------------------------
+    out = open(file.file_input_seqID + '.fa', 'w')
+#go through the sequencenames to be extracted-----------------------------------
+    indexList = findIndexOfSeq(file.file_lines_seqIDs)
+#get the sequences from the seqID file------------------------------------------
+    seq = extractSeqs(indexList)
+#write everything to file-------------------------------------------------------
     out.write(seq)
     out.close()
 
-    #------------------------------------------------------------
 if __name__ == "__main__":
     main()
