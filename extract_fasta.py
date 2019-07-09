b144ae66f08e60c6d349474b4a06cbc63c198261
zendl0r <d.zendler@gmx.de>
2019-07-09 10:09:45 -0400

    Update extract_fasta.py

--- extract_fasta.py
+++ extract_fasta.py
@@ -1,18 +1,19 @@
-#Python script to extract fasta sequences from multi-fasta files
+#Python script to extract fasta sequences from multi-fasta files.
+#Not useful for extracting larger numbers of reads from raw sequencing data.
+#This should be used to create subsets from gene or protein lists after using
+#signalP or targetP.
 #How-to use:
-#   python /path/to/script/extract_fasta.py /path/to/sequence/list/sequence_list.txt
-#   /path/to/fasta/file/fasta_file.fa
+#   python exSeqList.py
 #correspondance: d.zendler@gmx.de
 #Tested on python version 2.7.15_1, MacOS High Sierra 10.13.6
 
 from __future__ import print_function
 import sys, os, datetime, time
-#------------------------------------------------------------------------------
+
 #Taken from: https://www.agnosticdev.com/content/how-open-file-python
 #Matt Eaton on Sun, 12/17/2017 - 10:35 PM
 class File():
     def __init__(self):
-        #Initializing the variables for the file
         self.file_lines_seqIDs = []
         self.file_lines_multiFasta = []
         self.file_input_1 = ""
@@ -24,7 +25,6 @@ class File():
         self.working_path = ""
         self.sequence_name = ""
 
-        #Check if there is a filename indicated
         self.parse_stdin()
         if self.file_input_seqID is not "" or self.file_input_stdin1 is not None:
             self.read_content_of_file()
@@ -43,15 +43,24 @@ class File():
     def parse_stdin(self):
         if len(sys.argv) == 3 and sys.argv[1] is not None:
             self.file_input_seqID = sys.argv[1]
+            if self.file_input_seqID == "h" or self.file_input_seqID == "-h" or self.file_input_seqID == "-help" or self.file_input_seqID == "help":
+                print(" _________________________________________________________________________________________")
+                print("|Extract sequences from a multi Fasta file using a sequence name list.                    |\n|")
+                print("|Type: python seqIDs_file multiFasta.fa                                                   |\n|")
+                print("|If still in desperate need for help see: https://github.com/zendl0r/sequence_annotation  |")
+                print(" ----------------------------------------------------------------------------------------- ")
+                exit()
             self.file_input_multiFasta = sys.argv[2]
             self.working_path = os.getcwd()
         elif not sys.stdin.isatty():
             self.file_input_stdin1 = sys.stdin
         else:
-            print("A problem occured.\n")
-            print("Calling the script: python seqIDs_file multiFasta.fa\n")
-            print("Contact: d.zendler@gmx.de")
-            exit("Please provide a valid list of seqIDS and a valid Fasta file")
+            print(" _________________________________________________________________________________________")
+            print("|Extract sequences from a multi Fasta file using a sequence name list.                    |\n|")
+            print("|Type: python seqIDs_file multiFasta.fa                                                   |\n|")
+            print("|If still in desperate need for help see: https://github.com/zendl0r/sequence_annotation  |")
+            print(" ----------------------------------------------------------------------------------------- ")
+            exit()
 
     def read_content_of_file(self):
         try:
@@ -71,20 +80,31 @@ class File():
         except ValueError:
             error_raised = "Error loading file: " + str(sys.exc_info()[0])
             print("This program needs an input file to continue. Exiting")
-#function to find the index of the sequences to be extracted--------------------
+
 def findIndexOfSeq(seqID):
+    """function to find the index of the sequences to be extracted"""
     file = File()
     indexList = []
+    count = 0
     for line in seqID:
+        count = count + 1
+        sys.stdout.write("indexing progress: %d%%   \r" % (count))
+        sys.stdout.flush()
         if line != "":
-            line = ">" + line
+            if '>' not in line:
+                line = ">" + line
             indexList.append(file.file_lines_multiFasta.index(line))
     return indexList
-#function to extract the sequences----------------------------------------------
+
 def extractSeqs(indexList):
+    """function to extract the sequences"""
     file = File()
     seq = ''
+    count = 0
     for ind in indexList:
+        count = count + 1
+        sys.stdout.write("extraction progress: %d%%   \r" % (count))
+        sys.stdout.flush()
         seq = seq + file.file_lines_multiFasta[ind] + "\n"
         ind = ind + 1
         while ind < len(file.file_lines_multiFasta):
@@ -94,19 +114,14 @@ def extractSeqs(indexList):
             else:
                 break
     return seq
-#get the job done---------------------------------------------------------------
+
 def main():
-#Call the current date and time-------------------------------------------------
     now = datetime.datetime.now()
-#Call the input files-----------------------------------------------------------
     file = File()
-#Generating the outputfile------------------------------------------------------
     out = open(file.file_input_seqID + '.fa', 'w')
-#go through the sequencenames to be extracted-----------------------------------
     indexList = findIndexOfSeq(file.file_lines_seqIDs)
-#get the sequences from the seqID file------------------------------------------
     seq = extractSeqs(indexList)
-#write everything to file-------------------------------------------------------
+    print(seq)
     out.write(seq)
     out.close()
 
